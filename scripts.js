0e0a9c0115a979a2c763874f8d62d396d1a27492
SohelRana2015 <contactsohelrana@gmail.com>
2022-05-20 19:32:04 +0600

    this is a simple Bulb on of task

---
+++ scripts.js
@@ -0,0 +1,11 @@
+document.getElementById('on').addEventListener('click', function(){
+
+    document.getElementById('onoff').src="pic_bulbon.gif";
+
+});
+
+document.getElementById('off').addEventListener('click', function(){
+
+    document.getElementById('onoff').src="pic_bulboff.gif";
+
+});
